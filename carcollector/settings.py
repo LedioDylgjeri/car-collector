628ab836151c6d5a3787fd4b8c8eb17cc67878ef
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-11-14 10:23:11 -0500

    Config deployment

--- carcollector/settings.py
+++ carcollector/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zh3a#8kglw4(31-q_&d8l*oy1-@i%6@m0h47)e&qly1+u41)lw'
+# SECRET_KEY = 'django-insecure-zh3a#8kglw4(31-q_&d8l*oy1-@i%6@m0h47)e&qly1+u41)lw'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
