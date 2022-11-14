224e24b2dbb853bfc19472c9d0e827837743b4c3
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-11-14 10:33:04 -0500

    Config deployment

--- carcollector/settings.py
+++ carcollector/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-zh3a#8kglw4(31-q_&d8l*oy1-@i%6@m0h47)e&qly1+u41)lw'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -142,8 +141,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Configure Django App for Heroku.
 import django_heroku
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 # Other settings above
 django_heroku.settings(locals())
 
-DEBUG_PROPAGATE_EXCEPTIONS = True
 
